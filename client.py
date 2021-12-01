af51d2a99dff2b17178c0e0f8b2e7670f5a1decb
Ronda <agusleonel.r@gmail.com>
2021-11-30 22:55:51 -0300

    Corrections

--- client.py
+++ client.py
@@ -1,14 +1,10 @@
 import socket
 import json
-import colorama
-from colorama import Fore
-
 
 PORT = 8080
 IPV4 = '192.168.2.138'
 FORMAT = 'UTF-8'
 HEADER = 16
-DISPLAY_DIMENSION = (240, 240)
 
 def colored(r, g, b, text):
     return "\033[38;2;{};{};{}m{} \033[38;2;255;255;255m".format(r, g, b, text)
@@ -22,14 +18,15 @@ addr = (IPV4, PORT)
 client = socket.socket()
 client.connect(addr)
 
-
-msg = input("Number between 1-898: ").encode(FORMAT)     #Pedir mensaje al usuario
+msg = 0
+while 1 < msg and msg > 899:
+    msg = input("Number between 1-898: ").encode(FORMAT)     #Pedir id del pokemon
+    
 len_msg = len(msg)          
 header = str(len_msg)
 header += ' ' * (HEADER - len(header))    #Calcular y rellenar espacios del mensaje del cliente
 
 send_msg(client, msg, header)
-
 
 lenght = client.recv(500).decode(FORMAT)
 
@@ -60,10 +57,7 @@ for i in range(int(image_info_json['height'])):
         break
 
 if show_image:
-
-    inicio_x = round((DISPLAY_DIMENSION[0] - image_info_json['width']) / 2)
-    inicio_y = round((DISPLAY_DIMENSION[1] - image_info_json['height']) / 2)
-
+    
     for row in rows:
         # print(row['row'][0])
         for i in range(len(row['row'])):
